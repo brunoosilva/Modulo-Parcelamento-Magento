9e8583249b7c22c776ae6446c80102dd03506dc2
Bruno Oliveira <321.bruno@gmail.com>
2015-04-01 16:53:27 -0300

    Ajustes Parcelamento e Desconto Boleto
    Suportando a exibição de parcelas nas listagens de produto. Adiciona a
    exibição do desconto no boleto para pagamento á vista.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# Módulo Parcelamento Magento
+# Módulo Parcelamento & Boleto Magento
 
-Módulo que exibe as formas de parcela da loja, na tela de detalhe do produto.
+Módulo que exibe as formas de parcela da loja e desconto no pagamento em boleto, na tela de detalhe do produto e listagens dos produtos.
+
+Obs.: Lembrando que o módulo só faz a exibição dos valores nas telas de listagem e detalhe do produto, porem os valores não são 
+contabilizados ou integrados com o checkout, ainda. :D
 
 # Instalação
 
@@ -11,4 +14,14 @@ Módulo que exibe as formas de parcela da loja, na tela de detalhe do produto.
 3 - Use este código "<?php echo $this->getChildHtml('Facilestore_Quota_ProductQuota') ?>" para chamar o módulo dentro da página do produto 
     <i>(/app/design/frontend/base/default/template/catalog/product/view.phtml)</i>
 	
-4 - Para configurar o Módulo: Sistema > Configurações > Facile Store Extensions > Parcelas Produtos
+4 - Para funcionar a exibição nas listagens de produtos, segue os procedimentos abaixo:
+
+	4a - Abra o arquivo <i>(/app/design/frontend/base/default/template/catalog/product/list.phtml)</i>
+	4b - Adicione este código "<?php $this->getChild('Facilestore_Quota_ProductQuota')->setData("product", $_product); ?>"
+		 abaixo da linha "<?php foreach ($_productCollection as $_product): ?>" ~Linha 95 (Format List)
+	4c - Adicione este código "<?php $this->getChild('Facilestore_Quota_ProductQuota')->setData("product", $_product); ?>"
+		 abaixo da linha "<?php foreach ($_productCollection as $_product): ?>" ~Linha 216 (Format Grid)
+	4d - Adicione o código "<?php echo $this->getChildHtml('Facilestore_Quota_ProductQuota') ?>" abaixo de todos este código 
+		"<?php echo $this->getPriceHtml($_product, true); ?>"
+	
+5 - Para configurar o Módulo: Sistema > Configurações > Facile Store Extensions > Parcelas Produtos
--- app/code/local/Facilestore/Quota/Block/Productquota.php
+++ app/code/local/Facilestore/Quota/Block/Productquota.php
@@ -8,29 +8,54 @@ class Facilestore_Quota_Block_Productquota extends Mage_Catalog_Block_Product_Ab
         $this->_configs['qtdparcelas'] = Mage::getStoreConfig('facilestore_quota/general/qtdparcelas');
         $this->_configs['valorminimoparcela'] = Mage::getStoreConfig('facilestore_quota/general/valorminimoparcela');
         $this->_configs['juroscartao'] = Mage::getStoreConfig('facilestore_quota/general/juroscartao');
+        $this->_configs['boleto_enable'] = Mage::getStoreConfig('facilestore_quota/general/boleto_enable');
+        $this->_configs['porcentagem_boleto'] = Mage::getStoreConfig('facilestore_quota/general/porcentagem_boleto');
+        $this->_configs['txt_boleto'] = Mage::getStoreConfig('facilestore_quota/general/txt_boleto');
     }
     
     public function isEnable(){
         return $this->_configs['enable'];
     }
     
-    public function getParcelas(){
+    public function getDescBoleto(){
+        if($this->_configs['boleto_enable'] && $this->_configs['porcentagem_boleto']){
+            $product = $this->_getProduct();
+            if($product){
+                $percent = $this->_configs['porcentagem_boleto'];
+                $txt_boleto = $this->_configs['txt_boleto'];            
+                $valor = $product->getFinalPrice() - ($product->getFinalPrice() * ($percent / 100));            
+                $txt_boleto = str_replace("#percent#", $percent, $txt_boleto);
+                $txt_boleto = str_replace("#valor#", $this->_formatPrice($valor), $txt_boleto);
+                return $txt_boleto;    
+            }            
+        }
+        return false;
+    }
+    
+    public function getParcelas($list=false){
         $product = $this->_getProduct();
         $parcelas = array();
-        for($i=1; $i<=$this->_configs['qtdparcelas']; $i++):
-            $parcela = $product->getFinalPrice() / $i;
-            if($parcela < $this->_configs['valorminimoparcela'] && $this->_configs['juroscartao']>0){
-                $juroscartao = str_replace(",", ".", $this->_configs['juroscartao']);
-                $valor_final = $product->getFinalPrice() * pow((1 + ($juroscartao/100)), $i);
-                $parcela = $valor_final / $i;
-            }
-            $return = array(
-                'numero' => $i,
-                'valor_parcela' => $this->_formatPrice($parcela),
-                'valor_parcela_total' => $this->_formatPrice($i * $parcela),
-            );
-            $parcelas[] = $return;
-        endfor;
+        if($product){            
+            for($i=1; $i<=$this->_configs['qtdparcelas']; $i++):
+                $parcela = $product->getFinalPrice() / $i;
+                if($parcela < $this->_configs['valorminimoparcela'] && $this->_configs['juroscartao']>0){
+                    $juroscartao = str_replace(",", ".", $this->_configs['juroscartao']);
+                    $valor_final = $product->getFinalPrice() * pow((1 + ($juroscartao/100)), $i);
+                    $parcela = $valor_final / $i;
+                }
+                $return = array(
+                    'numero' => $i,
+                    'valor_parcela' => $this->_formatPrice($parcela),
+                    'valor_parcela_total' => $this->_formatPrice($i * $parcela),
+                );
+                $parcelas[] = $return;
+            endfor;
+        }
+        
+        if($list){
+            return array_pop($parcelas);
+        }
+        
         return $parcelas;
     }
     
@@ -44,5 +69,13 @@ class Facilestore_Quota_Block_Productquota extends Mage_Catalog_Block_Product_Ab
     
     public function _formatPrice($price){
         return Mage::helper('core')->currency($price, true, false);
+    }
+    
+    public function isCatalog(){
+        $onCatalog = true;
+        if(Mage::registry('current_product')) {
+            $onCatalog = false;
+        }
+        return $onCatalog;
     }
 }
